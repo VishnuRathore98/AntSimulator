270b5fb789a1cb033ae8a9733389613d9cef75d5
VishnuRathore98 <rathorevishnu666@gmail.com>
2026-02-26 23:31:45 +0530

    Refactor ant simulation to handle two ants with separate movement logic

--- ant_sim.py
+++ ant_sim.py
@@ -14,10 +14,15 @@ pygame.display.set_caption("Ant Simulator")
 
 clock = pygame.time.Clock()
 
-ant_x = WIDTH // 2
-ant_y = HEIGHT // 2
-ant_angle = random.uniform(0, 2 * math.pi)
-ant_speed = 2
+ant1_x = WIDTH // 2
+ant1_y = HEIGHT // 2
+ant1_angle = random.uniform(0, 2 * math.pi)
+ant1_speed = 2
+
+ant2_x = WIDTH // 2
+ant2_y = HEIGHT // 2
+ant2_angle = random.uniform(0, 2 * math.pi)
+ant2_speed = 2
 
 running = True
 
@@ -28,19 +33,33 @@ while running:
         if event.type == pygame.QUIT:
             running = False
 
-    ant_angle += random.uniform(-0.1, 0.1)
-
-    ant_x += math.cos(ant_angle) * ant_speed
-    ant_y += math.sin(ant_angle) * ant_speed
-
-    if ant_x < 0 or ant_x > WIDTH:
-        ant_angle = math.pi - ant_angle
-    if ant_y < 0 or ant_y > HEIGHT:
-        ant_angle = -ant_angle
-
     screen.fill((0, 0, 0))
 
-    pygame.draw.circle(screen, (255, 255, 255), (int(ant_x), int(ant_y)), 5)
+    # ant 1
+    ant1_angle += random.uniform(-0.1, 0.1)
+
+    ant1_x += math.cos(ant1_angle) * ant1_speed
+    ant1_y += math.sin(ant1_angle) * ant1_speed
+
+    if ant1_x < 0 or ant1_x > WIDTH:
+        ant1_angle = math.pi - ant1_angle
+    if ant1_y < 0 or ant1_y > HEIGHT:
+        ant1_angle = -ant1_angle
+
+    pygame.draw.circle(screen, (255, 255, 255), (int(ant1_x), int(ant1_y)), 5)
+
+    # ant 2
+    ant2_angle += random.uniform(-0.1, 0.1)
+
+    ant2_x += math.cos(ant2_angle) * ant2_speed
+    ant2_y += math.sin(ant2_angle) * ant2_speed
+
+    if ant2_x < 0 or ant2_x > WIDTH:
+        ant2_angle = math.pi - ant2_angle
+    if ant2_y < 0 or ant2_y > HEIGHT:
+        ant2_angle = -ant2_angle
+
+    pygame.draw.circle(screen, (255, 255, 255), (int(ant2_x), int(ant2_y)), 5)
 
     pygame.display.flip()
 
